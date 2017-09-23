97dae030e5a455690d14bcdf1df9792af5fc7ff7
Sandy Yeung <sandynism@gmail.com>
2017-09-23 00:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const parents = document.querySelectorAll('.ranked-list')
 
-  for (let a = 0; b = parents.length; a < b; a++) {
+  for (let a = 0, b = parents.length; a < b; a++) {
     let children = parents[a].children
 
-    for (let c = 0; d = children.length; c < d; c++) {
+    for (let c = 0, d = children.length; c < d; c++) {
       children[c].innerHTML = parseInt(children[c].innerHTML) + n
     }
   }
