19e8dac774ca0f98936584343bacaf385ba2ab18
Sandy Yeung <sandynism@gmail.com>
2017-09-23 00:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+  const parents = document.querySelectorAll('.ranked-list')
   
+  for(let a = 0; b = parents.length; a < b; a++) {
+    let children = parents[a].children
+
+    for (let c = 0; d = children.length; c < d; c++) {
+      children[c].innerHTML = parseInt(children[c].innerHTML) + n
+    }
+  }
 }
